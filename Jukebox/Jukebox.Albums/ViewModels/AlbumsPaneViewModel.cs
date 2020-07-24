11edfc4e2feaf2e3ea9fdc26d46745682c73fef3
Josh Kunz <engndeveloper@gmail.com>
2020-07-23 23:16:17 -0500

    Saving changes

--- Jukebox/Jukebox.Albums/ViewModels/AlbumsPaneViewModel.cs
+++ Jukebox/Jukebox.Albums/ViewModels/AlbumsPaneViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Messaging;
 using Jukebox.Shared.ViewModels;
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using Jukebox.Shared.Classes;
 using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Jukebox.Albums.ViewModels
 {
@@ -14,16 +16,29 @@ namespace Jukebox.Albums.ViewModels
 
         public AlbumsPaneViewModel(IMessenger messenger)
         {
-            _messenger.Register<IEnumerable<SongViewModel>>(this, Messages.AllSongsLoadedSendToAlbums, );
+            _messenger = messenger ?? throw new ArgumentNullException(nameof(messenger));
+
+            //_messenger.Register(this, );
         }
 
-
         #region Properties
-        public ObservableCollection<>
+        public ObservableCollection<AlbumViewModel> Albums
+        {
+            get => _albums;
+            set => Set(ref _albums, value);
+        }
+        private ObservableCollection<AlbumViewModel> _albums;
+
+        private ImageSource AlbumCoverImage
+        {
+            get => _albumCoverImage;
+            set => Set(ref _albumCoverImage, value);
+        }
+        private ImageSource _albumCoverImage;
         #endregion
 
         #region Commands
-
+        public ICommand ShowAllAlbumSongs { get; set; }
         #endregion
 
         #region Command Methods
@@ -34,6 +49,11 @@ namespace Jukebox.Albums.ViewModels
 
         #endregion
 
-
+        #region Message Methods
+        public void AllSongsLoadedToAlbums(NotificationMessage<IEnumerable<SongViewModel>> message)
+        {
+            MessageBox.Show("allsongsloadedtoalbums");
+        }
+        #endregion
     }
 }
